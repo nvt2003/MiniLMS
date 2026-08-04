e0854a13295cdffa8a66402bf13589d0862946d7
Thu <thunvhe176252@fpt.edu.vn>
2026-08-04 14:55:07 +0700

    add authenication

--- LMS/src/routes/authRoutes.js
+++ LMS/src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const AuthController = require('../controllers/authController');
-const { verifyToken, restrictTo} = require('../middlewares/authMiddleware');
+const { verifyToken, restrictTo, requireAdmin} = require('../middlewares/authMiddleware');
 
 router.post('/register', AuthController.register);
 
