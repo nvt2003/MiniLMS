565ac1cdcedd87eacec854f3e10afff20a20856c
Thu <thunvhe176252@fpt.edu.vn>
2026-08-04 16:59:58 +0700

    add auth for sub admin

--- LMS/src/routes/settingRoutes.js
+++ LMS/src/routes/settingRoutes.js
@@ -5,12 +5,12 @@ const { verifyToken,restrictTo } = require('../middlewares/authMiddleware');
 
 router.get("/group/:group", SettingController.getSettings);
 router.get("/key/:key", SettingController.getSettingByKey);
-router.put('/key/:key',verifyToken,restrictTo('admin'), SettingController.updateByKey);
-router.put('/group/:group',verifyToken,restrictTo('admin'), SettingController.updateByGroup);
-router.post('/',verifyToken,restrictTo('admin'), SettingController.createSetting);
+router.put('/key/:key',verifyToken,restrictTo('admin','admin_pages'), SettingController.updateByKey);
+router.put('/group/:group',verifyToken,restrictTo('admin','admin_pages'), SettingController.updateByGroup);
+router.post('/',verifyToken,restrictTo('admin','admin_pages'), SettingController.createSetting);
 router.get("/group/:group/:key", SettingController.getSettingsByGroupAndKey);
 router.get("/group", SettingController.searchGroup);
 router.get("/parent", SettingController.searchParent);
-router.put("/",verifyToken,restrictTo('admin'), SettingController.updateSettingValue);
+router.put("/",verifyToken,restrictTo('admin','admin_pages'), SettingController.updateSettingValue);
 
 module.exports = router;
