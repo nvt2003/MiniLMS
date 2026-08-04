2637e55214f81fc6d6230d5ab77324c0f7fe6de6
Thu <thunvhe176252@fpt.edu.vn>
2026-08-04 15:44:12 +0700

    fix confirm alert

--- LMS_fe/src/Components/Navbar.jsx
+++ LMS_fe/src/Components/Navbar.jsx
@@ -73,11 +73,11 @@ const Navbar = () => {
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
-  const handleLogout = () => {
-    confirm("Đăng xuất", "Bạn muốn đăng xuất khỏi hệ thống?", () => {
+  const handleLogout = async () => {
+    if (await confirm("Đăng xuất", "Bạn muốn đăng xuất khỏi hệ thống?")) {
       localStorage.clear();
       navigate("/login");
-    });
+    }
   };
   useEffect(() => {
     const fetchNavbarConfig = async () => {
